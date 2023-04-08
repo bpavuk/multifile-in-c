ea04f5159a77eea808082614ee331e1de6b264fc
bpavuk <boopentiy@gmail.com>
2023-04-08 17:17:54 +0300

    Initial commit, added getFileSize function instead of hello world

---
+++ library.c
@@ -0,0 +1,25 @@
+#include "library.h"
+
+#include <stdio.h>
+
+/**
+ * @param filename - name of file
+ * @return file size in bytes, -1 if error happened
+ */
+long getFileSize(const char *filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    if (size == -1) {
+        perror("Error getting file size");
+    }
+
+    fclose(fp);
+
+    return size;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef MULTIFILE_IN_C_LIBRARY_H
+#define MULTIFILE_IN_C_LIBRARY_H
+
+extern long getFileSize(const char *filename);
+
+#endif //MULTIFILE_IN_C_LIBRARY_H
